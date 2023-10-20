ef8cabb2f23c105a7120eda64840f54284f13f3c
Eyuel Haile <eyuelhaile29@gmail.com>
2023-10-20 12:10:43 +0300

    init

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,53 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
 
 export default function Home() {
+  const cart  = [{
+    id : 1,
+    title : "Mens Black SHoes",
+    price : 1200
+    
+  },{
+    id : 2,
+    title : "Womens red dress",
+    price : 3000
+    
+  },{
+    id : 3,
+    title : "Mens Watches",
+    price : 5000
+    
+  }]
+  const handleChapa = async () => {
+		const referenceNumber = uuidv4();
+		const header = {
+            headers: { 
+                "Content-Type": "application/json"
+             },
+            
+        };
+		
+		const body = {
+			amount: cart.reduce((total, item) => total + item.price, 0), //Amount should be integer
+			currency: "ETB",
+			email: "test@gmail.com",
+			first_name: "Eyuel",
+			last_name: "Haile",
+			phone_number: "0917000000", //the phone number must not include +251
+			tx_ref: referenceNumber,
+			callback_url: "http://localhost:3000/success",
+			return_url: "http://localhost:3000/varifyChapa?tnx_ref="+referenceNumber,
+			customization: {
+				title: "Item Payment",
+				description: "Paying for item in the cart"
+			}
+		}
+		let response = await axios.post(`http://localhost:3000/api/chapa`, body, header);
+		//console.log(response.data.resp.data.checkout_url);
+		window.location.href = response.data.data.checkout_url;
+	}
   return (
     <div className={styles.container}>
       <Head>
@@ -10,122 +56,39 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className={styles.card}>
+            <h3>Checkout Page </h3>
+            {cart.map((item) =>  (
+              <div className={styles.title} style={{justifyContent:"space-between"}}>
+                <span style={{float:"left"}}>{item.title}</span>
+                <span style={{float:"right"}}>{item.price} ETB</span>
+              </div>
+            ))}
+            <div className={styles.title} style={{justifyContent:"space-between"}}>
+                <span style={{float:"left"}}>Total</span>
+                <span style={{float:"right"}}>{cart.reduce((total, item) => total + item.price, 0)} ETB</span>
+            </div>
+            <button
+              type="button"
+              className={styles.btn}
+              onClick={handleChapa}
+            >
+              Pay with 
+              <img
+                  src="/images/chapa.svg"
+                  alt="chapa image"
+                  style={{
+                    width: "100px",
+                    paddingLeft: "15px",
+                    paddingTop: "5px"
+                  }}
+                />
+            </button>
+          </div>
         </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   );
 }
